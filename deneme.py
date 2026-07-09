14118fbd3b8fb83a1995f1370b079478747eb644
1bilalll <barslan2021@gtu.edu.tr>
2026-07-09 23:22:44 +0300

    new project added about functions

---
+++ deneme.py
@@ -0,0 +1,31 @@
+
+def calculate_love_score():
+    score=0
+    score2=0
+    name1=input("can you give name: ")
+    name2=input("can you give name: ")
+    name1_liste=list(name1)
+    name2_liste=list(name2)
+    
+    for i in name1_liste:
+        if i=="t":
+            score +=1  
+        if i=="r":
+            score +=1  
+        if i=="u":
+            score +=1  
+        if i=="e":
+            score +=1 
+        
+    for j in name2_liste:
+        if j=="l":
+            score2 +=1  
+        if j=="o":
+            score2 +=1
+        if j=="v":
+            score2 +=1
+        if j=="e":
+            score2 +=1
+        
+    print(f"Love Score :{score+score2}")
+calculate_love_score()
---
+++ hangman.py
@@ -0,0 +1,42 @@
+import random
+from hangman_words import word_lists
+from hangman2 import logo
+
+lives=6
+print(logo)
+chosen_words =random.choice(hangman_words.word_lists)
+print(chosen_words)
+placeholder =""
+word_length =len(chosen_words)
+for position in range(word_length):
+    placeholder +="_"
+print("word to guess"+placeholder)
+
+game_over =False
+correct_letters =[]
+
+while not game_over:
+    
+    guess =input("guess a letter:").lower()
+    if guess in correct_letters:
+        print(f"you'v already guessed{guess}")
+    display = ""
+    for letter in chosen_words:
+        if letter ==guess:
+            display +=letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display +=letter
+        else:
+            display += "_"
+    print("word to guess: "+display)
+
+    if guess not in chosen_words:
+        lives -=1
+        if lives==0:
+            game_over =True
+            print("*****you lose***")
+    if "_" not in display:
+        game_over =True
+        print("*****youwin***")
+    print(stages[lives])        
